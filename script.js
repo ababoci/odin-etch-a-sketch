a62fcc5fd4379aaa0f77faa863119f6adeb71898
Ardit Baboci <ardit.baboci@gmail.com>
2023-02-14 21:21:15 -0700

    Create 4x4 grid

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ container = document.querySelector(".container")
 
 for (let i = 0; i < 16; i++) {
     new_div = document.createElement("div");
+    new_div.classList.add("grid");
     container.append(new_div);
 }

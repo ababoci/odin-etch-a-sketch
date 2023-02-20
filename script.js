6cec6898c71c268c2ca4c0e472d022735efd55a3
Ardit Baboci <ardit.baboci@gmail.com>
2023-02-19 18:19:49 -0700

    Add ability to draw using rainbow colors

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const container = document.querySelector(".container")
-
 const colorSelector = document.querySelector("#penColor")
+const rainbowColors = document.querySelector("#rainbowColorsCheckbox")
 
 let mousedown = false;
 
 function changeColor(gridBlock) {
+    
+    if (rainbowColors.checked) {
+        const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+        const r = randomBetween(0, 255);
+        const g = randomBetween(0, 255);
+        const b = randomBetween(0, 255);
+        const randomRGB = `rgb(${r},${g},${b})`; // Collect all to a css color string
+    
+        gridBlock.style.backgroundColor = randomRGB;
+        return;
+    }
+
     gridBlock.style.backgroundColor = colorSelector.value;
 }
 
